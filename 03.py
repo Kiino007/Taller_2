944719eb5a8b894c1bf0110ab3054e5ddee7faeb
Kiino007 <kino007kevin@gmail.com>
2024-12-16 21:37:35 -0500

    Taller 2
    Ejercicio 3

--- 03.py
+++ 03.py
@@ -1,2 +1,12 @@
 #Escribe un programa que solicite al usuario un número entero positivo y luego calcule 
 #la suma de todos los números divisibles por 3 desde 1 hasta ese número utilizando un bucle while.
+
+num = int(input("Ingrese un numero entero y positivo: "))
+ini = 1
+suma = 0
+
+while ini <= num :
+    if ini % 3 == 0:
+      suma += ini
+    ini += 1
+print("La suma de los numeros multiplos de 3 es de es de: ", suma)
