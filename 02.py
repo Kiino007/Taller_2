7bf55609f2240d8cdfe43c89f28ea0fa79f6451c
Kiino007 <kino007kevin@gmail.com>
2024-12-16 21:01:33 -0500

    Punto 2
    Realizacion del punto 2

--- 02.py
+++ 02.py
@@ -1,3 +1,15 @@
 #Escribe un programa que solicite al usuario dos números enteros positivos y luego imprima 
 #todos los números entre ellos (inclusive) utilizando un bucle while.
 
+print("Ingrese dos numero enteros positivos en el orden indicado")
+
+num1 = int(input("Ingrese el numero mayor: "))
+num2 = int(input("Ingrese el numero menor: "))
+dif = num1 + 1
+
+while dif > num2:
+    if num2 != dif:
+        print(num2)
+    num2 += 1
+    
+    
