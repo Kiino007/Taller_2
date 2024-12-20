b98d8ac8712eec946dad229c347777e3225a1f71
Kiino007 <kino007kevin@gmail.com>
2024-12-19 23:21:51 -0500

    Taller 2
    Puntos finales del taller

--- 14.py
+++ 14.py
@@ -1,3 +1,15 @@
 # Escribe un programa que solicite al usuario un número entero positivo y luego 
 #imprima la suma de los números de Fibonacci hasta ese número utilizando un bucle while.
 
+numero = int(input("Introduce un número entero y positivo: "))
+
+a = 0
+b = 1
+suma = 0
+
+
+while a <= numero:
+    suma = suma + a
+    a, b = b, a + b
+
+print(f"La suma de los números de Fibonacci hasta {numero} es: {suma}")
--- 15.py
+++ 15.py
@@ -1,2 +1,17 @@
 # Escribe un programa que solicite al usuario un número entero positivo y luego 
 #imprima el promedio de todos los números desde 1 hasta ese número utilizando un bucle while.
+
+numero = int(input("Introduce un número entero positivo: "))
+
+suma = 0
+contador = 1
+
+
+while contador <= numero:
+    suma = suma + contador  
+    contador = contador + 1  
+
+promedio = suma / numero
+
+
+print(f"El promedio de los números desde 1 hasta {numero} es: {promedio}")
